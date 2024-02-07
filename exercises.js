c51aae68cf8cf84c5c0cec51f9bb32d29da8eeed
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-06 18:32:00 -0600

    function createAnimal with inner function/methods added

--- exercises.js
+++ exercises.js
@@ -19,4 +19,19 @@ const instructorA = {
         return this.firstName + " says bye!";
     }
 }
-console.log(instructorA(sayBye()));
+function createAnimal (species, verb, noise) {
+    return {
+        species,
+        [verb](){
+        return noise;
+    }
+  }
+}
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+
