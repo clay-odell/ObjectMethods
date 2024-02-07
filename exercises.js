100ae6de0bb47bb41e57eb27865318ddecf01886
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-06 18:22:40 -0600

    instructorA Say Hi, Say Bye added

--- exercises.js
+++ exercises.js
@@ -10,3 +10,13 @@ const instructor = {
     firstName: 'Colt',
     [favNum]: "That is my favorite!"
 }
+const instructorA = {
+    firstName: 'Colt',
+    sayHi(){
+        return "Hi"
+    },
+    sayBye(){
+        return this.firstName + " says bye!";
+    }
+}
+console.log(instructorA(sayBye()));
